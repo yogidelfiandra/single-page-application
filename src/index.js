acd404801b35ff12a21d7f766cd63d106c1ae4d8
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2023-05-02 20:42:41 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function HomePage() {
+  return <p>This is Homepage</p>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function AboutPage() {
+  return <p>This is About page</p>;
+}
+
+function FAQPage() {
+  return <p>This is FAQ page</p>;
+}
+
+function Link({ target, navigate, children }) {
+  return (
+    <a
+      href={target}
+      onClick={(event) => {
+        event.preventDefault(); // menghapus behavior default
+        navigate(target); // mengubah state berdasarkan target
+      }}
+    >
+      {children}
+    </a>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi state
+    this.state = {
+      page: '/',
+    };
+
+    this.navigate = this.navigate.bind(this);
+  }
+
+  navigate(target) {
+    this.setState(() => {
+      return {
+        page: target,
+      };
+    });
+  }
+
+  render() {
+    // if (this.state.page === '/') {
+    //   return <HomePage />;
+    // }
+    // if (this.state.page === '/about') {
+    //   return <AboutPage />;
+    // }
+    // if (this.state.page === '/faq') {
+    //   return <FAQPage />;
+    // }
+
+    // Short-circuit evaluation (&&)
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link target='/' navigate={this.navigate}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link target='/about' navigate={this.navigate}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link target='/faq' navigate={this.navigate}>
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {this.state.page === '/' && <HomePage />}
+          {this.state.page === '/about' && <AboutPage />}
+          {this.state.page === '/faq' && <FAQPage />}
+        </main>
+      </>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
